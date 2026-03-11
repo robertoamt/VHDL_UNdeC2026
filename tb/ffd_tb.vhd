047b8fba9c5173a2dc2fbe893e4206040f1093e9
rmillon <rmillon@undec.edu.ar>
2026-03-11 14:01:24 -0300

    working progress ffd_tb

---
+++ tb/ffd_tb.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity ffd_tb is --tb --> entidad vacia, no tiene E/S
+end entity ffd_tb;
+
+architecture sim of ffd_tb is
+--declarativa señales y componentes
+begin
+--mapeo de componente y estimulos
+
+end architecture
+
+
+
