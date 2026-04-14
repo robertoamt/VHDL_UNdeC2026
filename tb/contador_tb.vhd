ba4b4f0c91903df0d515e1a0139d5e9a60106e17
Carpioramiro20 <ramiro.ramirocarpio@gmail.com>
2026-04-14 06:57:12 -0300

    actualice codigo contador_tb

--- tb/contador_tb.vhd
+++ tb/contador_tb.vhd
@@ -1,3 +1,6 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
 entity contador_tb is
 end entity contador_tb;
 
@@ -9,4 +12,53 @@ architecture test_contador of contador_tb is
     );
     end component;
 
-    signal tb
+    signal clk, rst : STD_LOGIC := '0';
+    signal q : STD_LOGIC_VECTOR (3 downto 0);
+
+    constant Tclk : time := 10 ns;
+
+begin
+
+    -- Instanciación del contador
+    uut: contador
+        port map (
+            clk => clk,
+            reset => reset,
+            q => q
+        );
+
+    -- Generador de reloj
+    clk_process : process
+    begin
+        while true loop
+            clk <= '0';
+            wait for Tclk/2;
+            clk <= '1';
+            wait for Tclk/2;
+        end loop;
+    end process;
+
+    -- Estímulos
+    stim_proc: process
+    begin
+        -- Reset inicial
+        reset <= '1';
+        wait for 20 ns;
+        reset <= '0';
+
+        -- Dejar contar un tiempo
+        wait for 200 ns;
+
+        -- Aplicar reset otra vez
+        reset <= '1';
+        wait for 20 ns;
+        reset <= '0';
+
+        -- Seguir contando
+        wait for 100 ns;
+
+        -- Fin de simulación
+        wait;
+    end process;
+
+end Behavioral;  
